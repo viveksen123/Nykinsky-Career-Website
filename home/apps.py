2a1b2250e3be2c1ac20a513b9e14d77f58fa5d42
Vivek sen <131164811+viveksen123@users.noreply.github.com>
2025-07-05 00:34:53 +0530

    Update apps.py

--- home/apps.py
+++ home/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
