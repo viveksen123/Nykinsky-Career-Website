838b5b2ebf592affddc617ffdf116e8e39f06a51
Vivek sen <131164811+viveksen123@users.noreply.github.com>
2025-08-16 15:40:17 +0530

    Update settings.py

--- Nykinsky/settings.py
+++ Nykinsky/settings.py
@@ -78,13 +78,14 @@ WSGI_APPLICATION = 'Nykinsky.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'nykinsky',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'NAME': 'nykinsky_db',
+        'USER': 'nykinsky_user',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
